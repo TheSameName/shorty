3b6803b3fa8e917018565249ea594a627124b9d5
Gabriel S. <thesouzagab@gmail.com>
2023-10-30 19:22:37 -0300

    refactor: find method

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,11 +1,11 @@
 class RedirectsController < ApplicationController
-  before_action :find_link
+  before_action :find_link!
 
   def show; end
 
   private
 
-  def find_link
+  def find_link!
     @link = Link.find_by!(slug: params[:slug])
   end
 end

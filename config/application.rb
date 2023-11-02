8847c7507819dfdced6e02b5ffa2b1438aee5cfd
Gabriel S. <thesouzagab@gmail.com>
2023-11-02 09:54:28 -0300

    chore: enable active storage

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
+require "active_storage/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems

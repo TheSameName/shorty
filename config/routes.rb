b9db27fa642b2a452ecefa4510bd739476c658a9
Gabriel S <41161023+souzagab@users.noreply.github.com>
2023-11-13 21:32:48 -0300

    Add challenge on forms (#12)
    * Add turnstile on forms
    
    * chore: enable auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,25 +8,24 @@ Rails.application.routes.draw do
   get  "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
-  if Rails.env.local?
-    get  "sign_up", to: "registrations#new"
-    post "sign_up", to: "registrations#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
-    resources :sessions, only: %i[index show destroy]
-    resource  :password, only: %i[edit update]
+  resources :sessions, only: %i[index show destroy]
+  resource  :password, only: %i[edit update]
 
-    namespace :identity do
-      resource :email,              only: %i[edit update]
-      resource :email_verification, only: %i[show create]
-      resource :password_reset,     only: %i[new edit create update]
-    end
-
-    resources :links
-
-    get "/home" => "home#index"
+  namespace :identity do
+    resource :email,              only: %i[edit update]
+    resource :email_verification, only: %i[show create]
+    resource :password_reset,     only: %i[new edit create update]
   end
 
+
+  resources :links
+
   get "/statistics" => "statistics#index"
+
+  get "/home" => "home#index"
 
   root "links#new"
 
